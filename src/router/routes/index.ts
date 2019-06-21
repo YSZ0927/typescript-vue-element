62f8ff4bca0871cf7c7cd15dcbcbcb1188e83704
YuXun <chaufany@gmail.comom>
2019-06-21 15:20:04 +0800

    新增登录页面

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,9 +1,10 @@
 import testpage1 from './testPage1';
 import testpage2 from './testPage2';
+import Login from './login';
 
 
 export default [
     ...testpage2,
     ...testpage1,
-
+    ...Login,
 ];
